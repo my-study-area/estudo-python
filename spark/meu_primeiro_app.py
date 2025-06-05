88f3302ad4b91694df3af4405feff2452b4642e8
Adriano Avelino <adriano-st777@hotmail.com>
2025-06-04 22:08:37 -0300

    adiciona tipagem na aplicação spark

--- spark/meu_primeiro_app.py
+++ spark/meu_primeiro_app.py
@@ -1,5 +1,8 @@
 # meu_primeiro_app.py
-from pyspark.sql import SparkSession
+from typing import Tuple
+
+import pyspark.sql
+from pyspark.sql import SparkSession, DataFrame
 
 def print_local(mensagem :str) -> None:
     print("=============================================\n")
@@ -10,7 +13,7 @@ if __name__ == "__main__":
     # Inicializa a SparkSession.
     # .appName define o nome do seu aplicativo no Spark UI.
     # .master("local[*]") diz ao Spark para rodar localmente, usando todos os núcleos disponíveis.
-    spark = SparkSession.builder \
+    spark: SparkSession = SparkSession.builder \
         .appName("MeuPrimeiroAppLocal") \
         .master("local[*]") \
         .getOrCreate()
@@ -18,26 +21,26 @@ if __name__ == "__main__":
     print_local("SparkSession criada com sucesso!")
 
     # 1. Crie um DataFrame simples
-    dados = [("João", 25, "Engenheiro"),
+    dados: list[Tuple[str, int, str]] = [("João", 25, "Engenheiro"),
              ("Maria", 30, "Médica"),
              ("Pedro", 22, "Estudante"),
              ("Ana", 35, "Professora")]
 
-    colunas = ["Nome", "Idade", "Profissao"]
+    colunas: list[str] = ["Nome", "Idade", "Profissao"]
 
-    df = spark.createDataFrame(dados, colunas)
+    df: DataFrame = spark.createDataFrame(dados, colunas)
 
     print_local("DataFrame Original:")
     df.show()
 
     # 2. Faça uma operação Spark: Filtrar por idade
-    df_filtrado = df.filter(df.Idade > 28)
+    df_filtrado: DataFrame = df.filter(df.Idade > 28)
 
     print_local("DataFrame Filtrado (Idade > 28):")
     df_filtrado.show()
 
     # 3. Faça outra operação Spark: Contar registros
-    total_registros = df.count()
+    total_registros: int = df.count()
     print_local(f"Total de registros no DataFrame original: {total_registros}")
 
     # 4. Parar a SparkSession. Isso é importante para liberar recursos.
