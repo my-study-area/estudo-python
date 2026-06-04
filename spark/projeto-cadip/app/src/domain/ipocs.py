677e6cd326c392fd98e7e34f6e12ca7a3eafa422
Adriano Avelino <adriano-st777@hotmail.com>
2026-06-04 17:02:20 -0300

    feat: adiciona classes para ipocs

---
+++ spark/projeto-cadip/app/src/domain/ipocs.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
+
+class Ipocs:
+    def __init__(self, data_frame: DataFrame) -> None:
+        self.__data_frame = data_frame
+
+    def to_df(self) -> DataFrame:
+        return self.__data_frame.select(
+            col("numero_contrato_servico").cast("string").alias("numero_contrato"),
+            col("codigo_ipoc").cast("string").alias("codigo_ipoc")
+        )
---
+++ spark/projeto-cadip/app/src/domain/ipocs_fake_extract.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from awsglue.context import GlueContext
+from pyspark.sql import SparkSession
+from src.domain.ipocs import Ipocs
+from src.extract import IExtract
+
+class IpocsFakeExtract(IExtract[Ipocs]):
+    def __init__(self, glue_context: GlueContext) -> None:
+        self.spark_session: SparkSession = glue_context.spark_session
+
+    def extract(self) -> Ipocs:
+        caminho_base = Path(__file__).parent.parent
+        arquivo_json = str(caminho_base / "dados_ipocs.json")
+        df = (
+            self.spark_session.read
+            .option("multiline", "true")
+            .json(arquivo_json)
+        )
+        return Ipocs(df)
