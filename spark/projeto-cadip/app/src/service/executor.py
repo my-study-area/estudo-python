3d9aa525a6940ed0c48bf537b9a8eb5f758c1516
Adriano Avelino <adriano-st777@hotmail.com>
2026-06-05 22:34:36 -0300

    refactor: refatora Executor passando print do df para método centralizado

--- spark/projeto-cadip/app/src/service/executor.py
+++ spark/projeto-cadip/app/src/service/executor.py
@@ -1,4 +1,4 @@
-from src.service.extract_builder import ExtractBuilder
+from src.etl.extract_builder import ExtractBuilder
 from src.service.glue_configuration import GlueConfiguration
 
 
@@ -9,9 +9,24 @@ class Executor:
 
     def run(self) -> None:
         print('running executor ...')
+        self.__show_all_data_frames()
+
+    def __show_all_data_frames(self):
         extract_contrato = self.__builder.build_extract_contrato()
         extract_contrato.extract().to_df().show()
 
         extract_posicoes_diarias = self.__builder.build_extract_posicoes_diarias()
         extract_posicoes_diarias.extract().to_df().show()
+
+        extract_participantes = self.__builder.build_extract_participantes()
+        extract_participantes.extract().to_df().show()
+
+        extract_ipocs = self.__builder.build_extract_ipocs()
+        extract_ipocs.extract().to_df().show()
+
+        extract_identificacao_pessoas = self.__builder.build_extract_identificao_pessoas()
+        extract_identificacao_pessoas.extract().to_df().show()
+
+        extract_dados_cadastrais = self.__builder.build_extract_dados_cadastrais()
+        extract_dados_cadastrais.extract().to_df().show()
 
